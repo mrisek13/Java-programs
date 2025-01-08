f06d9099fe2d3b9bae6f265b7690dd351a267eea
Maja Risek <108302992+mrisek13@users.noreply.github.com>
2025-01-08 23:48:47 +0100

    Create Oglas.java

---
+++ 09 - JDBC/Oglas.java
@@ -0,0 +1,43 @@
+package oglascrud;
+
+public class Oglas {
+    private int id;
+    private String naslov;
+    private String datum;
+    private String tekst;
+
+    public Oglas() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaslov() {
+        return naslov;
+    }
+
+    public void setNaslov(String naslov) {
+        this.naslov = naslov;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+
+    public String getTekst() {
+        return tekst;
+    }
+
+    public void setTekst(String tekst) {
+        this.tekst = tekst;
+    }
+}
+
